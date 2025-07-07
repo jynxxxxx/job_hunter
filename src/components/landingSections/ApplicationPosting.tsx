c1244a2b20dbb78533b2ef871f501053ac1b00ea
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-07 22:30:16 +0900

    fix landing page announcement

--- src/components/landingSections/ApplicationPosting.tsx
+++ src/components/landingSections/ApplicationPosting.tsx
@@ -3,7 +3,7 @@ const ApplicationPosting = () => {
   return (
     <p className="flex flex-col gap-4 text-md z-10 dont-bold text-white text-center bg-bright py-4 px-8 sm:rounded-4xl w-[fit-content] sm:text-xl sm:mx-auto md:text-3xl">
       <div>현재 <strong>포스코스틸리온 대졸 신입</strong>(7/3 - 7/12) 전용 AI 자기소개서 서비스 시작</div> 
-      <div>현재 <strong>SK하이닉스 신입 수시채용</strong>(7/7 - 7/16) 전용 AI 자기소개서 서비스 시작</div>
+      {/* <div>현재 <strong>SK하이닉스 신입 수시채용</strong>(7/7 - 7/16) 전용 AI 자기소개서 서비스 시작</div> */}
     </p>
   );
 };
