ad09aa81f77bbd796ccd09374cd4ab3809038995
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-10 15:34:51 +0900

    update banner

--- src/components/landingSections/ApplicationPosting.tsx
+++ src/components/landingSections/ApplicationPosting.tsx
@@ -25,15 +25,26 @@ const ApplicationPosting = () => {
 
   return (
     <div className={styles.announcementBanner}>
-      <p>
-        현재 <strong className={styles.strongText}>포스코스틸리온, SK하이닉스, LG에너지솔루션</strong> 신입사원 수시채용 전용 AI 자기소개서 서비스 시작
-      </p>
-      <a href="/dashboard" className={styles.ctaButton}>
-        자세히 알아보기
-      </a>
-      <button className={styles.dismissButton} onClick={handleDismiss}>
-        &times;
-      </button>
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+        <div className="w-full sm:w-4/5">
+          <div className="flex justify-center items-center gap-4">
+            <p>
+              현재 <strong className={styles.strongText}>SK하이닉스, LG에너지솔루션, 포스코스틸리온</strong> 신입사원 수시채용 전용 AI 자기소개서 서비스 시작
+            </p>
+          </div>
+          <div className="pt-4 text-[1.4rem] lg:text-[1.7rem]">
+            금요일 7월11일 23:59까지 <strong>모든 AI 자기소개서 무료! </strong>
+          </div>
+        </div>
+        <div className="ml-auto md:ml-0">
+          <a href="/dashboard" className={styles.ctaButton}>
+            자세히 알아보기
+          </a>
+          <button className={styles.dismissButton} onClick={handleDismiss}>
+            &times;
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
