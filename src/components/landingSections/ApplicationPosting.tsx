9ce9e62ae238b37ed501449d58a8c11825b2a750
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-11 17:58:45 +0900

    update banner

--- src/components/landingSections/ApplicationPosting.tsx
+++ src/components/landingSections/ApplicationPosting.tsx
@@ -25,24 +25,25 @@ const ApplicationPosting = () => {
 
   return (
     <div className={styles.announcementBanner}>
+      <div className="ml-auto md:ml-0 absolute top-2 right-4">
+        <button className={styles.dismissButton} onClick={handleDismiss}>
+          &times;
+        </button>
+      </div>
       <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
         <div className="w-full sm:w-4/5">
-          <div className="flex justify-center items-center gap-4">
+          <div className="flex justify-center items-center gap-4 text-[1.3rem]">
             <p>
-              현재 <strong className={styles.strongText}>SK하이닉스, LG에너지솔루션, 포스코스틸리온</strong> 수시채용 맞춤 서비스 제공
+              <strong className={styles.strongText}>SK하이닉스, LG에너지솔루션 </strong> <span className="text-[1rem]">등</span>
             </p>
           </div>
-          <div className="pt-4 text-[1.4rem] lg:text-[1.7rem]">
-            7월11일 금요일 23:59까지 <strong>AI 자기소개서 서비스 선착순 제공!</strong>
+          <div className="pt-4 text-[1rem] lg:text-[1.3rem]">
+            <strong>AI 자기소개서  서비스 선착순 30명 무료!</strong><br/>
+            <span className="text-[1rem] lg:text-[1.3rem]">7월11일 금요일 23:59까지</span> 
           </div>
-        </div>
-        <div className="ml-auto md:ml-0">
-          <a href="/dashboard" className={styles.ctaButton}>
-            자세히 알아보기
-          </a>
-          <button className={styles.dismissButton} onClick={handleDismiss}>
-            &times;
-          </button>
+            <div className="text-gray-400 text-[0.8rem] lg:text-[1rem]">
+              *이벤트 종료 후 정가 30,000원에 판매됩니다.
+          </div>
         </div>
       </div>
     </div>
