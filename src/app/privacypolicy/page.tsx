12683837337e3a33139cfbcca31d8592fe7e0979
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-23 15:34:38 +0900

    add privacy page for meta

---
+++ src/app/privacypolicy/page.tsx
@@ -0,0 +1,8 @@
+import PrivacyPolicy from "@/components/layoutSections/PrivacyForm";
+
+
+export default function PrivacyPolicyPage() {
+  return (
+    <PrivacyPolicy />
+  )
+}
--- src/components/layoutSections/Footer.tsx
+++ src/components/layoutSections/Footer.tsx
@@ -14,10 +14,10 @@ const Footer = () => {
               className="object-contain"
             />
           </div>
-          <span className={footerStyles.tagline}>지원서부터 면접까지, 바로지원이 도와드립니다.</span>
           <span className={footerStyles.tagline}>상호명: 바로지원㈜ | 사업자번호: 598-50-01044</span>
           <span className={footerStyles.tagline}>주소: 서울시 마포구 백범로 31길 21 서울창업허브</span>
           <span className={footerStyles.tagline}>대표이사: 박근철 | 이메일: teambarojiwon@gmail.com</span>
+          <span className={footerStyles.tagline}><a href="/privacypolicy"> 개인정보 처리방침</a></span>
         </div>
       </div>
       <div className={footerStyles.bottomSection}>
