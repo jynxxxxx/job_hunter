1e66141e8b10fda2a8d30eb227f671c6fabf2aad
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-27 10:06:32 +0900

    fix typos

--- src/components/genCharacteristics.tsx
+++ src/components/genCharacteristics.tsx
@@ -11,7 +11,7 @@ export default function GenCharacteristics() {
           <div className="font-extrabold text-gray-900 sm:text-center">AI 기반생성</div>
           <div className="text-gray-700 text-sm sm:text-center">
             최신 AI 기술로 기업과 직무에 최적화된<br/>
-            자기소개서를 갱겅합니다
+            자기소개서를 생성합니다
           </div>
         </div>
       </div>
@@ -34,7 +34,7 @@ export default function GenCharacteristics() {
         <div className="flex flex-col">
         <div className="font-extrabold text-gray-900 sm:text-center">간편한 사용</div>
           <div className="text-gray-700 text-sm sm:text-center">
-            복잡한 설정 없이 몇가지 정조와 답변을<br/>
+            복잡한 설정 없이 몇가지 정보와 답변을<br/>
             입력하면 바로 생성됩니다
           </div>
         </div>
