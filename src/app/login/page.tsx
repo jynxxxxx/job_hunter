8619eb0c20c1e3553502fa07c44f7d19c3bbf9f7
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-07-28 23:04:35 +0900

    fix the login page color

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/components/layoutSections/LoginForm';
 export default function LoginPage() {
 
   return (
-    <div className='h-[85vh] flex sm:items-center justify-center bg-primary'>
+    <div className='h-[85vh] flex sm:items-center justify-center bg-light'>
       <div className="h-fit bg-white w-xs my-24 sm:w-md mx-auto p-4 border rounded-md shadow-md">
       <LoginForm />
       </div>
